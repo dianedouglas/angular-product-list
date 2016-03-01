619c5f9a9b4efc5165a3028efc789ff67bff6c7d
diane <student@epicodus.com>
2016-03-01 15:05:57 -0800

    Add products list component with skeleton for its child component productrow. Pass the products array from the top level component to its child productslist.

--- app.js
+++ app.js
@@ -11,6 +11,74 @@ var browser_1 = require("angular2/platform/browser");
 var core_1 = require("angular2/core");
 // top level app component
 // this is the component decorator.
+// model
+var Product = (function () {
+    function Product(sku, name, imageUrl, department, price) {
+        this.sku = sku;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.department = department;
+        this.price = price;
+    }
+    return Product;
+}());
+/* The ProductsList component has two jobs:
+display all ProductRow components,
+and keep track of which one is selected */
+//inputs are data received from the parent component and stored as instance variables in the child component.
+/*
+Say we want to pass data stored in a parent component under the instance variable name 'myName' to a child component called my-component
+
+in the parent's template use the child including its inputs, just like a constructor:
+<my-component [shortName]="myName"></my-component>
+and also in the parent be sure to include it under directives (the controller class)
+directives: [ProductsList]
+
+Then in the child component:
+@Component({
+  selector: 'my-component',
+  inputs: ['name: shortName']
+})
+class MyComponent {
+  name: string;
+}
+this takes the value of the parent instance variable 'myName' and puts it into the child my-component
+under the input 'shortName', and the child component decorator takes that and puts it into an instance variable on the child controller class to match the attribute key 'name'
+the key in the inputs array shows how the data will be stored in the child controller
+the value is what variable the parent is putting the data into.
+these can be the same if you want a shortcut:
+
+<my-component [name]="myName"></my-component>
+inputs: ['name']
+class MyComponent {
+  name: string;
+}
+*/
+var ProductRow = (function () {
+    function ProductRow() {
+    }
+    ProductRow = __decorate([
+        core_1.Component({
+            selector: 'product-row',
+            template: "\n    <p>yo</p>\n  "
+        })
+    ], ProductRow);
+    return ProductRow;
+}());
+var ProductsList = (function () {
+    function ProductsList() {
+    }
+    ProductsList = __decorate([
+        core_1.Component({
+            selector: 'products-list',
+            directives: [ProductRow],
+            inputs: ['productList'],
+            template: "\n  <h3 *ngFor=\"#currentProduct of productList\">{{ currentProduct.name }}</h3>\n  "
+        })
+    ], ProductsList);
+    return ProductsList;
+}());
+//Top level component goes last. Needs to know about its children.
 var InventoryApp = (function () {
     function InventoryApp() {
         // when we make a new instance of the component, this constructor is called.
@@ -24,20 +92,10 @@ var InventoryApp = (function () {
     InventoryApp = __decorate([
         core_1.Component({
             selector: 'inventory-app',
-            template: "\n    <div class=\"inventory-app\">\n      <h1 *ngFor=\"#currentProduct of products\">{{ currentProduct.name }}</h1>\n    </div>\n  "
+            directives: [ProductsList],
+            template: "\n    <div class=\"inventory-app\">\n      <products-list [productList]=\"products\"></products-list>\n    </div>\n  "
         })
     ], InventoryApp);
     return InventoryApp;
 }());
 browser_1.bootstrap(InventoryApp);
-// model
-var Product = (function () {
-    function Product(sku, name, imageUrl, department, price) {
-        this.sku = sku;
-        this.name = name;
-        this.imageUrl = imageUrl;
-        this.department = department;
-        this.price = price;
-    }
-    return Product;
-}());
--- app.ts
+++ app.ts
@@ -4,11 +4,82 @@ import { bootstrap } from "angular2/platform/browser";
 import { Component } from "angular2/core";
 // top level app component
 // this is the component decorator.
+
+// model
+class Product {
+  constructor(
+    public sku: string,
+    public name: string,
+    public imageUrl: string,
+    public department: string[],
+    public price: number
+  ){}
+}
+
+/* The ProductsList component has two jobs:
+display all ProductRow components,
+and keep track of which one is selected */
+
+//inputs are data received from the parent component and stored as instance variables in the child component.
+/*
+Say we want to pass data stored in a parent component under the instance variable name 'myName' to a child component called my-component
+
+in the parent's template use the child including its inputs, just like a constructor:
+<my-component [shortName]="myName"></my-component>
+and also in the parent be sure to include it under directives (the controller class)
+directives: [ProductsList]
+
+Then in the child component:
+@Component({
+  selector: 'my-component',
+  inputs: ['name: shortName']
+})
+class MyComponent {
+  name: string;
+}
+this takes the value of the parent instance variable 'myName' and puts it into the child my-component
+under the input 'shortName', and the child component decorator takes that and puts it into an instance variable on the child controller class to match the attribute key 'name'
+the key in the inputs array shows how the data will be stored in the child controller
+the value is what variable the parent is putting the data into.
+these can be the same if you want a shortcut:
+
+<my-component [name]="myName"></my-component>
+inputs: ['name']
+class MyComponent {
+  name: string;
+}
+*/
+@Component({
+  selector: 'product-row',
+  template: `
+    <p>yo</p>
+  `
+})
+class ProductRow{
+
+}
+
+
+
+@Component({
+  selector: 'products-list',
+  directives: [ProductRow], // means we're using the component ProductRow in this template.
+  inputs: ['productList'], // array of strings = input keys to pass data like our array of products to this child component
+  template: `
+  <h3 *ngFor="#currentProduct of productList">{{ currentProduct.name }}</h3>
+  `
+})
+class ProductsList {
+  productList: Product[];
+}
+
+//Top level component goes last. Needs to know about its children.
 @Component({
   selector: 'inventory-app', //each of these is a key. lots of options for configuring a component.
+  directives: [ProductsList],
   template: `
     <div class="inventory-app">
-      <h1 *ngFor="#currentProduct of products">{{ currentProduct.name }}</h1>
+      <products-list [productList]="products"></products-list>
     </div>
   `
   // using the {{}} mustache tags is called template binding. Use value of expression inside brackets.
@@ -31,14 +102,3 @@ class InventoryApp {
 
 }
 bootstrap(InventoryApp);
-
-// model
-class Product {
-  constructor(
-    public sku: string,
-    public name: string,
-    public imageUrl: string,
-    public department: string[],
-    public price: number
-  ){}
-}
